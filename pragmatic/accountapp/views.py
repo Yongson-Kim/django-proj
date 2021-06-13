20fa931c59ce5914282bd3fac9b916b671297a6a
김용선 <yongson.kim@vntgcorp.com>
2021-06-13 20:54:44 +0900

    update

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -10,6 +10,7 @@ from django.views.generic import CreateView, DetailView, UpdateView
 from accountapp.models import HelloWorld
 
 def hello_world(request):
+
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')
 
@@ -20,8 +21,7 @@ def hello_world(request):
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hellow_world.html', context={'hello_world_list': hello_world_list})
-
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
