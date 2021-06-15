be035b4643ca5e9b02ee757b861658febef09973
김용선 <yongson.kim@vntgcorp.com>
2021-06-15 17:10:09 +0900

    udpate

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -34,3 +34,11 @@ class AccountCreateView(CreateView):
 class AccountDetailView(DetailView):
     model = User
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
